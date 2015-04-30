14e9406ea1293f2f82eed9bbd40eb7b873e8f27d
Emre Surmeli <s.emre.s.8@gmail.com>
2015-04-29 21:39:04 -0700

    editz

--- Q2/server.js
+++ Q2/server.js
@@ -1,19 +1,19 @@
-var http = require('http'),
-    fs = require('fs'),
-    host = '127.0.0.1',
-    port = '8000';
+var http = require('http');
+var fs = require('fs');
+var host = '127.0.0.1';
+var port = '8000';
 
 function Server (err, html) {
-    if (err) {
-        throw err;
-        console.log('There was an error, plz fix..')
-    }
-    http.createServer( function (req, res) {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.write(html);
-        res.end();
-    }).listen(port, host, function () {
-    console.log('Server running on http://' + host + ':' + port)
+  if (err) {
+    throw err;
+    console.log('There was an error, plz fix..')
+  }
+  http.createServer( function (req, res) {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(html);
+    res.end();
+  }).listen(port, host, function () {
+  console.log('Server running on http://' + host + ':' + port)
   })
 };
 
